adb66bac668fd0657d0ef1a7d4f227c88f769299
Norton Almeida <norton.almeida@gmail.com>
2025-05-21 14:11:55 -0300

    project-setup: removed unused imports

--- tests/app/page.test.tsx
+++ tests/app/page.test.tsx
@@ -1,11 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import HomePage from '@/app/page';
-import { describe, it, expect } from '@jest/globals';
+import { render, screen } from "@testing-library/react";
+import HomePage from "@/app/page";
 
-describe('HomePage', () => {
-  it('renders the home page', async () => {
+describe("HomePage", () => {
+  it("renders the home page", async () => {
     const { container } = render(<HomePage />);
-    const homePage = screen.getByTestId('home-page');
+    const homePage = screen.getByTestId("home-page");
     expect(homePage).toBeDefined();
     expect(container).toMatchSnapshot();
   });
