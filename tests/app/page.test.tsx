eaead50aca4869b11a47b78df2b1004a8f79e8d9
Norton Almeida <norton.almeida@gmail.com>
2025-05-21 14:14:36 -0300

    project-setup: put back imports

--- tests/app/page.test.tsx
+++ tests/app/page.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import HomePage from "@/app/page";
+import { describe, it, expect } from "@jest/globals";
 
 describe("HomePage", () => {
   it("renders the home page", async () => {
